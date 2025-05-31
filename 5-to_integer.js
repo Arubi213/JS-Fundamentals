f872bed05015d23a1c8a7d82e216c862413945c5
EMIO24 <emmanuel.osarodion@icloud.com>
2025-05-31 18:05:11 +0100

    sixth

---
+++ 5-to_integer.js
@@ -0,0 +1,8 @@
+const num = process.argv[2];
+const convertedNum = parseInt(num);
+
+if (isNaN(convertedNum)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${convertedNum}`);
+}
